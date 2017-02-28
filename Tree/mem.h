3e0eaf5061306b1f7054d90380e6f30eb9ada781
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-02-28 18:01:20 -0500

    Update mem.h

--- Tree/mem.h
+++ Tree/mem.h
@@ -1,1 +1,13 @@
+namespace tensorflow {
 
+Status ValidateMemoryTypes(DeviceType device_type, const Graph* g);
+Status EnsureMemoryTypes(DeviceType device_type, const string& device_name,
+                         Graph* g);
+
+
+Status MemoryTypeForOutput(DeviceType device_type, const Graph* g,
+                           const Node* n, int index, MemoryType* memory_type);
+
+}  
+
+#endif 
