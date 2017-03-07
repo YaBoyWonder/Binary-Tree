63a5067c32e6dbdf5582ef2277100842aebe4ec3
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-07 05:26:35 -0500

    Update bridge.mm

--- Tree/object/bridge.mm
+++ Tree/object/bridge.mm
@@ -2,7 +2,7 @@ int LuaObjcBridge::callObjcStaticMethod(lua_State *L)
 {
     if (lua_gettop(L) != 3 || !lua_isstring(L, -3) || !lua_isstring(L, -2))
     {
-       	lua_pushboolean(L, 0);
+        	lua_pushboolean(L, 0);
         	lua_pushinteger(L, kLuaBridgeErrorInvalidParameters);
     	return 2;
     }
